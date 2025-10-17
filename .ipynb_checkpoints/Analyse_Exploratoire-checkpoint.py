c029bb5c251e91094b29baa36cadedcd7687d52f
Didi17ane <didiane.koffi@epitech.eu>
2025-10-17 17:39:56 +0000

    [UPDATE]: vizzzzzzz

--- .ipynb_checkpoints/Analyse_Exploratoire-checkpoint.py
+++ .ipynb_checkpoints/Analyse_Exploratoire-checkpoint.py
@@ -2,7 +2,6 @@
 
 import streamlit as st
 import pandas as pd
-import time
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt 
@@ -11,28 +10,17 @@ from PIL import Image
 import plotly.express as px
 import plotly.graph_objects as go
 
-from typing import List, Tuple
 from datetime import date, time, datetime, timedelta
 from numerize.numerize import numerize
 from streamlit_elements import elements, mui, html
 import altair as alt
-
-import requests
-
-from streamlit_option_menu import option_menu
-from streamlit_navigation_bar import st_navbar
-import pages as pg
 
 import folium
 from streamlit_folium import st_folium
 
 import pydeck as pdk
 
-from Utilities.Sidebar import *
-from Utilities.globale import *
-from Utilities.fintech import *
 # ___________________________________________
-
 
 #### Page Configuration ####
 st.set_page_config(
@@ -72,29 +60,13 @@ st.markdown("""
 st.markdown('<h1 class="main-header">Analytics Dashboard</h1>', unsafe_allow_html=True)
 
 
-# --- Charger agrégats
-df_ehcvm = pd.read_csv("./DATAS/dataset_scoring_bancaire_fusion.csv") 
 
+# ------------------------ Charger les données
 
-# --- Pré-traitements
+df = pd.read_csv("../DATAS/CleanALL_EHCVM.csv")
+df_ehcvm = df.copy()
 
-prem_ligne = df_ehcvm.head()
-nbr_value = df_ehcvm.shape
-desc = df_ehcvm.describe(include='all')
-df_fin = df_ehcvm.dropna()
-null_val = df_fin.isna().sum()
-df_fin = df_fin.drop_duplicates()
-
-
-# --- Affichages en console
-
-print(f"\n***** Premieres lignes : \n {prem_ligne}")
-print(f"\n***** Nombres de lignes et colonnes : \n {nbr_value}")
-print(f"\n***** Description des variables : \n {desc}")
-print(f"\n***** Valeurs nulles : \n {null_val}")
-print(f"\n***** Dataset de traitement : \n {df_fin}")
-
-
+print(f"Dataset de visualisation : {df_ehcvm}")
 
 
 # ------------------------------------------------------ AFFICHAGE DANS LA PAGE --------------------------------------------------
@@ -102,19 +74,60 @@ print(f"\n***** Dataset de traitement : \n {df_fin}")
 # ------------------------- Ajout de la Sidebar
 
 with st.sidebar:
-    region,age_grp,sexe,mstat,risque,reco,data = sidebar(df_fin)
+    # st.image("", width=200)
+    st.title("Dashboard DIATA.ai")
+    
+    st.divider()
+
+    # Bases de données
+    st.subheader("Base de données")
+    database = st.multiselect(
+        "Database",
+        options=["EHCVM2021", ""],
+        default=["EHCVM2021"]
+    )
+    
+    # Filters
+    st.subheader("Filters")
+    
+    region = st.sidebar.selectbox("Région", ["Toutes"] + sorted(df_ehcvm["region"].unique().tolist()))
+    age_grp = st.sidebar.selectbox("Tranche d’âge", ["Toutes"] + sorted(df_ehcvm["age_grp"].unique().tolist()))
+    sexe = st.sidebar.selectbox("Sexe", ["Tous"] + sorted(df_ehcvm["sexe"].unique().tolist()))
+    mstat = st.sidebar.selectbox("Statut matrimonial", ["Tous"] + sorted(df_ehcvm["mstat"].unique().tolist()))
+    
+    data = df_ehcvm.copy()
+    # if "Toutes" not in region:
+    #    data = data[data["region"].isin(region)]
+    if region != "Toutes":
+        data = data[data["region"] == region]
+    if age_grp != "Toutes":
+        data = data[data["age_grp"] == age_grp]
+    if sexe != "Tous":
+        data = data[data["sexe"] == sexe]
+    if mstat != "Tous":
+        data = data[data["mstat"] == mstat]
+    
+    
+    # Advanced options
+    st.subheader("Advanced Options")
+    show_targets = st.checkbox("Show Targets", value=True)
+    show_forecasts = st.checkbox("Show Forecasts", value=False)
+    
+    st.divider()
+    st.markdown("© 2025 DIATA AFRICA SAS")
 
 # ------------------------ PAGE PRINCIPALE
 
     
 # ------------------------------ KPIs
 # KPIs
+
 st.subheader("Key Performance Indicators")
 col1, col2, col3, col4 = st.columns(4)
 
 
 with col1:
-    population = data["nbr_indv"].sum()
+    population = len(data)
     st.metric(
         label="Total Population",
         value=f"{population:,.0f} ",   
@@ -129,12 +142,13 @@ with col1:
     
 with col2:
     df_femme = data[data["sexe"]=="Féminin"]
-    femme = df_femme["nbr_indv"].sum()
-    pourc_fem = round((femme / population)*100,1)
+    femme = len(df_femme)
+    
+    if femme != 0:
+        pourc_fem = round((femme / population)*100,1)
     st.metric(
         label="Total femmes",
         value=f"{femme:,.0f}",
-       
     )
     st.markdown('<div class="metric-card">', unsafe_allow_html=True)
     if femme == 0:
@@ -143,8 +157,10 @@ with col2:
 
 with col3:
     df_homme = data[data["sexe"]=="Masculin"]
-    homme = df_homme["nbr_indv"].sum()
-    pour_hom = round((homme / population)*100,1)
+    homme = len(df_homme)
+    
+    if homme != 0:
+        pour_hom = round((homme / population)*100,1)
     st.metric(
         label="Total hommes",
         value=f"{homme:,.0f} ",
@@ -155,13 +171,13 @@ with col3:
     st.metric("% Hommes", f"{pour_hom} %")
     
 with col4:
-    rev = round(data['mean_rev'].mean(), 0)
+    rev = round(data['rev_total_mois'].mean(), 0)
     if np.isnan(rev):
         rev=0    
     st.metric("Revenu moyen (FCFA)", f"{rev:,.0f}")
     
     st.markdown('<div class="metric-card">', unsafe_allow_html=True)
-    bank = round(data['mean_banked'].mean()*100, 1)
+    bank = round(data['bancarise'].mean()*100, 1)
     if np.isnan(bank):
         bank=0    
     st.metric("Bancarisation", f"{bank} %")
@@ -176,36 +192,106 @@ st.markdown('</div>', unsafe_allow_html=True)
 st.subheader("PERFORMANCES")
 st.markdown("")
 st.markdown("Cette vision globale permet d'identifier les zones fortes/faibles, profils majoritaires, contrastes majeurs. Incontournable avant tout projet de scoring.")
-tab1, tab2, tab3 = st.tabs(["Vue générale de la population", "KPIs locatifs & financiers", "Exploration interactive"])
+tab1, tab2, tab3 = st.tabs(["Vue générale de la population", "KPIs locatifs & financiers", "Exploration Score"])
 
-tab_region = tabRegion(data)
 
 # --------------------------- 1ere page ---------------------
-
 with tab1:
-    c1, c2 = st.columns(2)
+
+    # Vue globale de la population
+    
+    c1, c2= st.columns(2)
     with c1:
         # Pyramide des âges
         st.subheader(":green[**Pyramide des âges**]")
-        pyramide(data)
+        st.divider()
+        age_sex = data.groupby(["age_grp","sexe"])["region"].count().unstack().fillna(0)
+        age_sex.plot(kind="barh", stacked=True)
+        st.pyplot(plt)
     with c2:
         # Zoom région: répartition des effectifs et indicateurs
+        tab_region = data.groupby("region").agg(population=("region", "size"),
+                                            revenu_moy=("rev_total_mois","mean"),
+                                            bank_moy=("bancarise","mean")).reset_index()
+         
         st.subheader(":green[**Comparaison des régions - Population & Revenus**]")
-        fig = repartition(tab_region)
+        st.divider()
+        fig, ax = plt.subplots()
+        tab_region.set_index("region")[["population", "revenu_moy"]].plot(kind="bar", ax=ax, secondary_y="rev_moy")
+
         st.pyplot(fig)
             
     # Groupes d’âge : population et revenu médian
     st.subheader(":green[**Effectifs & revenu moyen par âge**]")
         
-    age_grp = data.groupby("age_grp").agg(population=("nbr_indv","sum"), revenu_moy=("mean_rev","mean")).reset_index()
+    age_grp = data.groupby("age_grp").agg(population=("region", "size"), revenu_moy=("rev_total_mois","mean")).reset_index()
     st.dataframe(age_grp)
 
-# --------------------------- 2e page ---------------------
+    ca1, ca2 = st.columns(2)
+    with ca1:
+        #Catégorie socioprofessionnelle
+        
+        st.subheader(":green[**Catégorie socioprofessionnelle**]")
+        st.divider()
+        csp_counts = data['csp'].value_counts()
+        palette = sns.color_palette("tab20", len(csp_counts))
+        
+        fig, ax = plt.subplots(figsize=(12, 6))
+        bars = ax.bar(csp_counts.index, csp_counts.values, color=palette)
+        
+        # Annoter chaque barre avec son effectif
+        for bar, value in zip(bars, csp_counts.values):
+            ax.text(
+                bar.get_x() + bar.get_width() / 2,
+                bar.get_height(),
+                f'{value:,}',  # format séparateur milliers
+                ha='center', va='bottom',
+                fontsize=12, fontweight='bold'
+            )
+        
+        ax.set_ylabel("Effectif", fontsize=15)
+        ax.set_xlabel("")
+        plt.xticks(rotation=45, ha='right', fontsize=13)
+        sns.despine()
+        plt.tight_layout()
+        
+        st.pyplot(fig)
+        
+    with ca2:
+        # Secteur institutionnel
+        
+        st.subheader(":green[**Secteur institutionnel**]")
+        st.divider()
+        branch_counts = data['branch'].value_counts()
+        palette = sns.color_palette("tab20", len(branch_counts))
+        
+        fig, ax = plt.subplots(figsize=(12, 6))
+        bars = ax.bar(branch_counts.index, branch_counts.values, color=palette)
+        
+        # Annoter chaque barre avec son effectif
+        for bar, value in zip(bars, branch_counts.values):
+            ax.text(
+                bar.get_x() + bar.get_width() / 2,
+                bar.get_height(),
+                f'{value:,}',  # format séparateur milliers
+                ha='center', va='bottom',
+                fontsize=12, fontweight='bold'
+            )
+        
+        ax.set_ylabel("Effectif", fontsize=15)
+        ax.set_xlabel("")
+        plt.xticks(rotation=45, ha='right', fontsize=13)
+        sns.despine()
+        plt.tight_layout()
+        
+        st.pyplot(fig)
+        
+# --------------------------- 2ere page ---------------------
 
 with tab2:
     
-    
-   # st.subheader(":green[**📈 Effectifs & Revenu moyen par tranche d’âge**]")
+    # KPIs locatifs & financiers
+
     # Répartition par région et indicateurs clés
     st.subheader(":green[**📈 Répartition régionale**]")
     st.divider()
@@ -218,7 +304,11 @@ with tab2:
         st.subheader(":green[**Revenu moyen par tranche d’âge**]")
         st.divider()
 
-        fig = revenu(data)
+        # Revenu moyen par tranche d’âge
+        fig, ax = plt.subplots()
+        data.groupby("age_grp")["rev_total_mois"].mean().plot(kind="bar", ax=ax, color="skyblue")
+        ax.set_ylabel("Revenu moyen (FCFA)")
+
         st.pyplot(fig)
 
     with c2:
@@ -227,7 +317,7 @@ with tab2:
         st.divider()
         
         fig, ax = plt.subplots()
-        data.groupby("age_grp")["mean_banked"].mean().plot(kind="bar", ax=ax, color=["red", "orange","blue","green","grey", "purple", "skyblue"])
+        data.groupby("age_grp")["bancarise"].mean().plot(kind="bar", ax=ax, color=["red", "orange","blue","green","grey", "purple", "skyblue"])
         ax.set_ylabel("Taux de bancarisation (%)")
         st.pyplot(fig) 
     
@@ -241,7 +331,7 @@ with tab2:
         st.divider()
         
         fig, ax = plt.subplots()
-        data[["empl_formel","assurance"]].mean().plot(kind="bar", ax=ax, color=["purple","red"])
+        data[["empl_formel","a_assurance"]].mean().plot(kind="bar", ax=ax, color=["purple","red"])
         ax.set_ylabel("Proportion (%)")
         st.pyplot(fig)
 
@@ -250,57 +340,56 @@ with tab2:
         st.subheader(":green[**🏠 Répartition statuts logement**]")
         st.divider()
         
-        fig, ax = plt.subplots()
-        statut_cols = ["proprio_titre","proprio_sans","locataire","autre_logement"]
-        data[statut_cols].mean().plot(
-            kind="bar", ax=ax, color=["green","orange","blue","grey"]
+        # Compter les effectifs par modalité de la variable 'logem'
+        statuts_logement = data['logem'].value_counts(dropna=False)
+        labels = statuts_logement.index  # ou map les int en labels si besoin
+        
+        colors = ["green","orange","blue","grey"]  # Personnalise ici
+        
+        fig, ax = plt.subplots(figsize=(7, 5))
+        wedges, texts, autotexts = ax.pie(
+            statuts_logement,
+            labels=labels,
+            autopct='%1.1f%%',
+            startangle=90,
+            colors=colors[:len(statuts_logement)],
+            textprops={'fontsize': 12}
         )
-        ax.set_ylabel("Proportion (%)")
-        st.pyplot(fig) 
+        ax.set_ylabel("")
+        
+        plt.tight_layout()
+        st.pyplot(fig)
+
 
     # --------------------------------------------------------
     
-            
-
 # --------------------------- 3e page ---------------------
-    
+
 with tab3:
+
+    # Exploration Score
+
     col1, col2, col3 = st.columns(3)
     
     with col1:
-        st.metric("Score moyen bancaire", round(data["score_bancaire_optimise"].mean(),1))
+        st.subheader("Score moyen")
     with col2:
-        st.metric("% Accords crédit (recommandé+conditionnel)", 
-                  round(100*data[data["recommandation_credit"].str.contains("ACCORD")]["nbr_indv"].sum()/max(1,data["nbr_indv"].sum()),1))
+        st.subheader("% Profils sécurisé")
     with col3:
-        st.metric("% Profils très à risque", 
-                  round(100*data[data["categorie_risque_bancaire"]=="Risque_Tres_Eleve"]["nbr_indv"].sum()/max(1,data["nbr_indv"].sum()),1))
+        st.subheader("% Profils très à risque")
 
-    c1, c2 = st.columns(2)
-    with c1:
-        # Visualisation de la distribution du score bancaire 
-        st.subheader("Distribution du score bancaire par profil")
-        fig, ax = plt.subplots()
-        data.groupby("categorie_risque_bancaire")["nbr_indv"].sum().plot(kind="bar", color="crimson", ax=ax)
-        ax.set_ylabel("Population")
-        ax.set_xlabel("Catégorie de risque")
-        st.pyplot(fig)
 
-    with c2:
-        # Mix & analyse scoring : origine du risque
-        st.subheader("Répartition des recommandations de crédit")
-        fig, ax = plt.subplots()
-        data.groupby('recommandation_credit')["nbr_indv"].sum().plot.pie(
-            autopct='%1.1f%%', ax=ax, colors=["green","orange","gray","red"])
-        ax.set_ylabel("")
-        st.pyplot(fig)
 
-    # ------------------------------------------
-    
-    # Exploration avancée : score par statut logement/profil emploi
-    st.subheader("Scoring bancaire moyen par statut de logement")
-    stats = data.groupby("statut_logement")["score_bancaire_optimise"].mean().sort_values()
-    st.bar_chart(stats)
+
+
+
+
+
+
+
+
+
+
 
 
 
